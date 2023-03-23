7c505233bc1114736a6afc4f866f3b5cf1f013ae
chachacha12 <chaminwoo369@gmail.com>
2023-03-23 18:35:57 +0900

    Baekjoon 10871

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,3 +1,22 @@
 #include <iostream>
+//#include <bits/stdc++.h>
 
 using namespace std;
+
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
+
+    cout << "cccc";
+
+    int n,x,t;
+    cin >>n >>x;
+
+    for(int i=0; i<n;i++){
+       cin >>t;
+       if(t<x) cout <<t<<' ';
+    }
+
+    return 0;
+}
+
