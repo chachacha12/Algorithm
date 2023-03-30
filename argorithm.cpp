2025fdade1b913378292a913d8e514e364b25e91
chachacha12 <chaminwoo369@gmail.com>
2023-03-30 20:28:55 +0900

    배열의 쓰임

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,40 +1,4 @@
-#include <iostream>
-
-using namespace std;
-
-/// 배열의 idx자리에 num을 추가하는 함수. len값은 배열의 최대길이임
-void insert(int idx, int num, int arr[], int& len){
-   
-    for(int i = len; i>idx; i--){
-        arr[i] = arr[i-1];
-    }
-    arr[idx] = num;
-    len++;  //배열의 길이가 늘어났기에 최종길이값 늘려준것
-}
-
-
-void erase(int idx, int arr[], int& len){
-
-    len--;  //배열 최종길이 줄여준것    
-    for(int i=idx; i<len; i++){
-        arr[i] = arr[i+1];
-    }
-
-}
-
-void printArr(int arr[], int& len{
-    for(int i=0; i< len; i++){
-        cout<<arr[i] << ' ';
-    }
-    cout << "\n\n";
-}
-
-
-
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-
-    return 0;
-}
+//배열 쓰임 : 
+// 1. 배열은 데이터를 자주 바꾸지않고 쌓아두고 싶을때 자주 사용됨
+// 2. 거의 대부분 문제에서 일단 입력값을 쌓아두고 시작하는 문제가 많기에 입력을 담아두려고 사용하곤함. 
+//    인덱스를 통해 데이터에 빠르게 접근하는 목적으로 배열 사용하면 효과적 
