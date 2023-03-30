4f80da879b108fac6c60e59bfcb86d06bfb64651
chachacha12 <chaminwoo369@gmail.com>
2023-03-31 02:50:38 +0900

    0x01강문제 배열풀이

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,23 +1,29 @@
 #include <iostream>
-
 using namespace std;
 
-//백준 10808번 알파벳 갯수 문제
 
-int freg[26]; //배열은 전역으로 선언하면 알아서 0으로 초기화됨. 지역변수로 선언하면 쓰레기값들 들어가서..초기화 따로 해줘야함
+/// 바킹독 0x01강 연습문제인 arr가 주어졌을때 그 중 2개수를 골랐을때 합이 100이 되는 수 있으면 1, 아니면 0을 출력하기 문제임
+///보통은 O(n제곱) 으로 생각하지만 배열쓰면 O(n)만에 해결가능
+
+int brr[1000];
+
+bool func2(int arr[], int n){
+    for(int i=0; i < n ;  i++){
+        if(brr[100- arr[i]] ==1)
+            return 1;
+      
+       brr[arr[i]] =1;
+    }
+    return 0;
+
+}
+
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
-    string s;
-    cin >> s;
-    for(auto c: s){
-        freg[c-'a']++;
-    }
-    for(auto n: freg){
-        cout<<n<<' ';
-    }
+    
     
 
     return 0;
