f903b6077bd175116cb3747431a86f57023286e2
chachacha12 <chaminwoo369@gmail.com>
2023-03-31 14:55:55 +0900

    baekjoon 배열 2577 숫자의 갯수

--- argorithm.cpp
+++ argorithm.cpp
@@ -2,29 +2,28 @@
 using namespace std;
 
 
-/// 바킹독 0x01강 연습문제인 arr가 주어졌을때 그 중 2개수를 골랐을때 합이 100이 되는 수 있으면 1, 아니면 0을 출력하기 문제임
-///보통은 O(n제곱) 으로 생각하지만 배열쓰면 O(n)만에 해결가능
-
-int brr[1000];
-
-bool func2(int arr[], int n){
-    for(int i=0; i < n ;  i++){
-        if(brr[100- arr[i]] ==1)
-            return 1;
-      
-       brr[arr[i]] =1;
-    }
-    return 0;
-
-}
-
+int arr[10];
+int a,b,c;
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
+    cin>>a>>b>>c;
     
-    
+    ///10으로 나눈 나머지를 구하면 일의 자리 수를 알 수 있음
+    //그 후 sum값은 10씩 나눠주면 모든 자릿수의 숫자값을 확인가능
+    int sum = a*b*c;
+
+    while(sum>0){
+        int n = sum%10;
+        arr[n]++;
+        sum/=10;
+    }
+
+    for(auto c: arr){
+        cout<<c<<'\n';
+    }
 
     return 0;
 }
