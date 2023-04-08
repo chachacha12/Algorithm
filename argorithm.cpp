ce91fe3d1b0a9f3bb6d78863300886321cdcd2aa
chachacha12 <chaminwoo369@gmail.com>
2023-04-09 02:02:28 +0900

    배열 1475 방번호

--- argorithm.cpp
+++ argorithm.cpp
@@ -2,41 +2,40 @@
 #include <math.h>
 #include <algorithm>
 
-
 using namespace std; 
 
-int arr[20] ={0};
+
+int arr[10] ={0};
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
 
     int n;
-    int six_nine=0;
-    int other=0;
 
-    cin>>n;
-    
-    
+    cin >>n;
+
     while(n>0){
         arr[n%10]++;
-        n/=10;
+        n /= 10; 
     }
-    
-    for(int i=0 ;i<10; i++){
-        if(i==6 || i==9){
+
+    int entire=0;
+
+    for(int i=0; i<10; i++){
+        if(i==6 ||i==9){
+
         }else{
-            if(other < arr[i]){
-                other=arr[i];
+            if(arr[i] > entire){
+                entire = arr[i];
             }
         }
     }
 
-    six_nine = (arr[6]+arr[9]+1)/2;
-    //six_nine += (arr[6]+arr[9])%2;
-    
-    other = max(other,six_nine);
-    cout<<other;
+
+    int six_nine = (arr[6]+arr[9]+1)/2;
+
+    cout << (max(entire, six_nine));
 
 
     return 0;
