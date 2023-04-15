1233cce1fea58c303cc57f345f3622317c810b8c
chachacha12 <chaminwoo369@gmail.com>
2023-04-15 14:42:24 +0900

    큐의 쓰임

---
+++ app.py
@@ -0,0 +1,15 @@
+print('ee')
+
+재고 = ['k3','k5', 'k7']
+
+if 'k3' in 재고 :
+    print('잇음')
+else : 
+    print('없음')
+
+
+for i in 재고 :
+    print(i)
+
+def 인사하기() :
+    print('ㅎㅇㅎㅇ')
--- argorithm.cpp
+++ argorithm.cpp
@@ -1,37 +1,22 @@
-#include <iostream>
-#include <math.h>
-#include <algorithm>
-#include <list>
-#include <stack> 
 
-using namespace std; 
+/*
 
-//스택 문제
+큐
+- STL queue는 코테에서 많이 사용함. 
+
+queue<int> Q;
+
+Q.push(10); //10
+Q.push(20); //10 20
+Q.push(30); //10 20 30
+cout<< Q.size() <<'\n'; //3
+if(Q.empty()) cout<< "Q is not empty";
+Q.pop(); //20 30
+cout << Q.front() << '\n'; //20
+cout << Q.back() << '\n'; //30
+Q.push(40); //20 30 40
+Q.pop(); //30 40
+cout<< Q.front() <<'\n'; //30
 
 
-int main(){
-    ios::sync_with_stdio(0);
-    cin.tie(0);
-
-    stack<int> st;
-    int n;
-    int result=0;
-
-    cin>>n;
-
-    while(n--){
-        int num;
-        cin>> num;
-        if(num == 0){
-            result-= st.top();
-            st.pop();
-        }else{
-            st.push(num);
-            result+=num;
-        }
-    }
-
-    cout<<result;
-    
-    return 0;
-}
+*/
