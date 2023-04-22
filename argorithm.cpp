36b8a42a0d25f5e37543f147a64f2efa3fbf677e
chachacha12 <chaminwoo369@gmail.com>
2023-04-22 15:22:30 +0900

    BFS 1926 그림

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,44 +1,84 @@
-/*
 
-BFS란? - 다차원 배열에서 각 칸을 방문할떄 너비를 우선으로 방문하는 알고리즘
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <list>
+#include <stack> 
+#include <queue>
+using namespace std; 
 
-원래 BFS는 그래프 자료구조에서..모든 노드를 방문할때 쓰는 알고리즘임
-
-
-BFS 진행과정
-----------------------------------------------
-1. 시작하는 칸을 큐에 넣고 방문했다는 표시를 남김
-2. 큐에서 원소를 꺼내어 그 칸에 상하좌우로 인접한 칸에 대해 3번을 진행
-3. 해당 칸을 이전에 방문했다면 아무것도 하지않고, 처음으로 방문했다면 방문했다는 표시를 남기고
-해당 칸을 큐에 삽입
-4. 큐가 빌때까지 2번을 반복
-
-    모든 칸이 큐에 1번씩 들어가므로 시간복잡도는 칸이 N개일때 O(N) 
+#define X first
+#define Y second
 
 
---------------------------------------------------
+int board[502][502];  //모두 0으로 초기화
+bool vis[502][502];  //방문여부 확인을 위함. 모두 0으로 초기화 되는듯?
 
-pair 
-BFS 구현전에 코드에서 쓰이게 될 STL 자료구조
-
-pair를 쓰면 2개의 자료구조를 묶어서 쓸 수 있음
-
-pair<int,int> t1 = make_pair(10,13);
-pair<int,int> t2 = {4,6};  //c++ 11 이상부터 가능
-
-cout<< t2.first << t2.second 
-
-if(t2 <t1) cout << "t2 > t3";   //pair는 대소관계가 정해져있음. 앞에 원소를 먼저 비교하고
-뒤에 원소 비교함
-
----> BFS 구현할때 큐에 좌표를 넣어야 하는데 그때 pair씀
-
---------------------------------------------------
-
-BFS 코드
-
-->정석적인 코드 확인했음. 달달외우기
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 
 
+int main(){
+    ios::sync_with_stdio(0);
+    cin.tie(0);
 
-*/
+    int n, m;
+    cin >>n>>m;
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m;j++){
+            cin >> board[i][j];
+        }
+    }
+
+    int mx=0; //계속 업뎃되는 가장 큰 그림의 크기
+    int num=0; //그림의 갯수
+
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+        
+            //그림이 없는 칸이거나 방문한적 있는 칸일경우엔 패스
+            if( board[i][j]==0 || vis[i][j]) 
+                continue;
+            
+            num++; //전체 그림 갯수 1증가
+            queue<pair<int, int>> Q;
+            int area=0; //현재 그림의 크기 저장 변수 
+            vis[i][j] = 1;
+            Q.push({i,j});
+
+            //하나의 그림을 반복문 돌면서 전부 방문함
+            while( !Q.empty()){
+                area++; //큐에 들은 원소를 하나 뺄때마다 넓이를 1증가
+                pair<int, int> cur = Q.front();
+                Q.pop();
+                ///상하좌우칸 확인 로직 
+                for(int i=0; i<4; i++){
+                    int x = cur.X + dx[i];
+                    int y = cur.Y + dy[i];
+                    
+                    //범위를 벗어나면 패스
+                    if( x<0 ||  x>=n || y<0 || y>=m)
+                        continue;
+                    
+                    //그림이 없거나 이미 방문한 칸이면 패스
+                    if( board[x][y] !=1 || vis[x][y]) 
+                        continue;
+
+                    vis[x][y] =1;
+                    Q.push({x,y});
+                }
+            }
+            //이전에 저장된 가장 큰 그림과 현재 확인한 그림의 크기를 비교하여 더 큰 값을 저장 
+            mx = max(area, mx);
+        }
+    }
+
+    cout<<num<<'\n'<<mx;
+
+
+    return 0;
+}
+
+
