a9f2fe1523f191f434b6e6fc67d0f3750511af91
chachacha12 <chaminwoo369@gmail.com>
2023-05-02 17:54:15 +0900

    BFS 2178 미로탐색

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,74 +7,61 @@
 #include <queue>
 using namespace std; 
 
-#define X first
-#define Y second
+#define X first 
+#define Y second 
 
-int board[502][502];
-bool vis[502][502];
+string board[102];
+
+int dist[102][102];
+
+
+
 
 int dx[4] = {1,0,-1,0};
 int dy[4] = {0,1,0,-1};
 
-int num=0; //그림갯수
-int mx=0; //젤 큰 그림 넓이
 
-
-int main(){
+int main(){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-
-    int n, m;
-    cin>>n>>m;
+    
+    int n,m;
+    cin>>n>>m; 
     
     for(int i=0; i<n;i++){
-        for(int j=0; j<m; j++){
-            cin>>board[i][j];
-        } 
+        cin>>board[i];
     }
 
-
+    
     for(int i=0; i<n;i++){
-        for(int j=0; j<m; j++){
-            if( vis[i][j]==1 ||  board[i][j]==0 ){
+        fill(dist[i], dist[i]+m, -1);
+    }
+
+    queue<pair<int, int>> Q;
+
+    Q.push({0,0});
+    dist[0][0]=1;
+
+    while(!Q.empty() ){
+        pair<int, int> p = Q.front();
+        Q.pop();
+
+        for(int i=0; i<4; i++){
+            int x = p.X +dx[i];
+            int y = p.Y +dy[i];
+
+            if(x < 0|| x>=n || y <0 || y>=m)
                 continue;
-            }
-            queue< pair<int,int> > Q;
 
-            num++;
-            Q.push( {i,j} );
-            vis[i][j] = 1;
+            if(board[x][y] !='1' ||  dist[x][y] != -1 )
+                continue;
 
-            int area=0; //현재 그림의 넓이 
-
-            while( !Q.empty()){
-                pair<int,int> p = Q.front();
-                Q.pop();
-                area++;
-
-                for(int i=0; i<4; i++){
-                    int x = p.X +dx[i];
-                    int y = p.Y +dy[i];
-
-                    if(x < 0 || x>=n  || y < 0 || y>=m)
-                        continue;
-
-                    if(board[x][y]==0 || vis[x][y] )
-                        continue;
-                    
-                    Q.push( {x,y});
-                    vis[x][y]=1;
-                }
-
-            }
-            mx = max(mx,area);
-
-        } 
+            Q.push({x,y});
+            dist[x][y] = dist[p.X][p.Y] +1;    
+        }
     }
 
-    cout<<num<<'\n'<<mx;
-    
-
+    cout<<dist[n-1][m-1];
 
     return 0;
 }
