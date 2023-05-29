184dbef9f032a2cf9316e6e27e79809a330ef687
chachacha12 <chaminwoo369@gmail.com>
2023-05-29 16:54:08 +0900

    BFS 1012 유기농배추(기본문제)

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,46 +7,83 @@
 #include <queue>
 using namespace std; 
 
-//BFS 응용4 - 1차원에서의 BFS
-
 #define X first 
-#define Y Second 
+#define Y second 
 
-int dist[100002]; 
-queue<int> Q;
-int dx[2] = {1, -1};
+int dist[52][52];
 
+int t;
+int m,n,k;
 
-int n,k;
+int sum;
+
+queue<pair<int,int>> Q;
+
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
+
 
 
 int main(){ 
     ios::sync_with_stdio(0);
     cin.tie(0);
-    
-    cin>>n>>k;  //n은 수빈이 위치, k는 동생위치 
 
-    fill(dist, dist+100001, -1);
-    
-    Q.push(n); 
-    dist[n] = 0;
 
-   while(dist[k] == -1 ){  // !Q.empty()하지말고 이렇게 해야 시작점과 정답지점이 같을때도 종료가됨. 
-        int cur = Q.front();
-        Q.pop();
+    cin>>t;
 
-        for(int num : {cur-1, cur+1, cur*2}){
-            
-             if(num<0 || num> 100000)  //범위 벗어나면 패스
-                continue;
-             if(dist[num]>=0) //이미 방문한 곳이면 패스
-                 continue;
-             
-             Q.push(num);
-             dist[num] = dist[cur]+1;
+    //테스트 케이스 수만큼 진행
+    while(t--){
+        sum = 0; //필요한 지렁이 갯수
+        for(int i=0; i<52; i++){
+             fill(dist[i], dist[i]+52, 0);  //판 초기화
         }
+       
+        cin>>m>>n>>k;
+        //존재하는 배추갯수만큼 반복
+        while(k--){
+            int x,y;
+            cin>>x>>y;
+            dist[x][y] = 1;
+        }
+
+        for(int i=0; i<m;i++){
+            for(int j=0; j<n;j++){
+                if(dist[i][j]==1){
+                    sum++;
+                    Q.push({i,j}); 
+                    dist[i][j] = 0; 
+
+                    while(!Q.empty()){
+                        auto cur = Q.front();
+                        Q.pop();
+
+                        for(int i=0;i<4;i++){
+                            int x = cur.X + dx[i];
+                            int y = cur.Y + dy[i];
+
+                            //범위를 벗어났다면 패스
+                            if(x<0 || x>=m || y <0 || y>=n ) 
+                                continue;
+                            
+                            if(dist[x][y]== 0) //이미 방문했거나 배추가 없는 곳이면 패스
+                                continue;
+
+                            Q.push({x,y});
+                            dist[x][y] = 0;
+                        }
+                    }
+   
+                }
+
+            }
+        }
+
+        cout<<sum<<'\n';
+
     }
-    cout<<dist[k];
+
+
+   
    
     return 0;
 }
