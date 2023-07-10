bf9c44f591a67ba99830acdbbe8b54e15037e394
chachacha12 <chaminwoo369@gmail.com>
2023-07-10 16:38:59 +0900

    2573 BFS 빙산(응용문제)

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,95 +6,105 @@
 #include <queue>
 using namespace std; 
 
-/*
-아래는 내가 직접 생각해서 짜본 코드이고
-바킹독 설명: https://www.youtube.com/watch?v=yPuow6aACvE
+#define X first 
+#define Y second 
 
--> 이 문제는 시간복잡도 널널하게 n제곱으로 풀어도된다면, 그냥 쉽게
-학생 하나하나 반복하면서 다시 자신에게로 돌아오는지만 체크해보면됨. 돌아오면 팀 있는거고
-안돌아오면 팀 없는것.  (사이클안에 포함되어 있으면 무조건 자기자신에게로 다시 돌아오기 때문) 
+int board[305][305];
 
-*/
+int dx[4] = {1,0,-1,0};
+int dy[4] = {0,1,0,-1};
 
 
+int n,m;
 
-int t;
+//몇년지났는지 세줌
+int years=0;
 
 int main(){ 
     ios::sync_with_stdio(0);  
     cin.tie(0); 
 
-    cin>>t;
+    cin>>n>>m;
 
-    //테스트케이스 수만큼 반복
-    while(t--){
-        int board[100002];
-        int dist[100002]; //0이면 아직 미방문. 1이면 방문한상태(스택에 들어있는상태). 2이면 팀 이미 이뤘거나 절대 못 이루는 상태
-        fill(board, board+100002,0);
-        fill(dist, dist+100002,0);
-        stack<int> s;
-        int n=0;
-        int chosen_num =0;
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            cin >> board[i][j];
+        }
+    }
 
-        cin>>n;
 
-        //학생들 수만큼 반복하며 각 학생들이 선택한 번호 입력
-        for(int i=1; i<=n; i++){
-            cin>>board[i];
+
+    //나뉜 덩어리가 2개이상이 될 동안 계속 진행 
+    while(1){ 
+        //변수들 초기화 
+        int vis[305][305] = {};
+
+        for(int i=0; i<305; i++){
+            fill(vis[i], vis[i]+305, 0);
         }
 
-        //팀 못이룬 학생수를 출력해줘야함
-        
-        //학생 한명한명 돌면서 만약 아직 미방문인 인덱스면 스택에 넣어줌
-        for(int i=1; i<=n;i++){
+        queue<pair<int,int>> q = {};
+        int count=0;  //빙산 덩어리 몇갠지 세줌
 
-                if(dist[i] == 2 ) {       //방문한 경우
+
+        //하나씩 돌면서 만약 빙산칸 나오면 BFS 진행
+        for(int i=0; i<n; i++){
+            for(int j=0; j<m; j++){
+                
+                //만약 0이거나 방문기록 있으면 패스 
+                if(board[i][j] ==0 || vis[i][j]==1)
                     continue;
-                }else{        //미방문인 경우
-                    s.push(i);
-                    dist[i] = 1; //스택에 들어가면 1
 
-                    while(!s.empty()){
-                        int index = s.top(); 
+                //빙산칸 발견
+                vis[i][j] = 1;
+                q.push({i,j});
+                count++;
 
-                        //미방문인 경우  - 계속해서 다음녀석들 방문해줄거
-                        if(dist[board[index]] == 0){
-                            s.push(board[index]);
-                            dist[board[index]] = 1;  //스택에 넣었다는 표시
 
-                        //방문해서 스택에 있는경우, 즉 사이클 만들어진것 (본인이 본인 선택한 경우도 포함) - 팀이뤄진거임
-                        }else if(dist[board[index]] ==  1){
+                while(!q.empty()){
+                    auto cur = q.front();
+                    q.pop();
 
-                            while(s.top() != board[index]){
-                                dist[ s.top() ] = 2;
-                                chosen_num++;
-                                s.pop();
-                            }
-                            dist[ s.top() ] = 2;
-                            chosen_num++;
-                            s.pop();
-                            
-                            while(!s.empty()){  //나머지 선택 못받은 녀석들도 다 비워주기
-                                dist[ s.top() ] = 2;
-                                s.pop();
-                            } 
+                    for(int i=0; i<4; i++){
+                        int x = cur.X + dx[i];
+                        int y = cur.Y + dy[i];
+                        
+                        //이미 방문한 칸이면 패스
+                        if(vis[x][y] ==1)
+                            continue;
 
-                        }else{  //이미 방문했던 경우 - 팀을 절대 만들 수 없음
-                            
-                            while(!s.empty()){  //이때끼지 스택에 쌓인 녀석들 다 방문표시
-                                dist[ s.top() ] = 2;
-                                s.pop();
-                            } 
+
+                        //board값이 0이하가 아니고 빙산칸 주변의 0인칸이라면
+                        if(board[x][y] == 0 && board[cur.X][cur.Y] !=0 ){
+                            board[cur.X][cur.Y]--;
+                        }
+
+                         //빙산칸이라면
+                        if(board[x][y] > 0){
+                            vis[x][y] =1;
+                            q.push({x,y});
                         }
                     }
                 }
+            }
         }
-         
-        int result = n - chosen_num;  //전체 학생수 - 선택받은 학생수
-        cout << result<<"\n";
+        //1년 지남
+        years++; 
+        
+        //덩어리가 분리되지 않고 다 녺으면
+        if(count == 0){
+            cout<< 0;
+            break;
+        }
+
+        //덩어리가 2개 이상일시
+        if(count >= 2){ 
+            cout << years-1;
+            break;
+        }
+      
     }
-    
-    
+
 
 
     return 0;
