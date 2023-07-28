053c141559ea832ad0165b1efd34ba7bf5b1e2c4
chachacha12 <chaminwoo369@gmail.com>
2023-07-29 01:20:00 +0900

    9663 백트래킹 N-Queen(연습)

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,37 +7,43 @@
 #include <queue>
 using namespace std; 
 
+//백트래킹은 n값이 작을때 진행하는 알고리즘인듯 
 
-//백트래킹의 기본적인 구조임. 잘 외워두기. 
+int n;
+int cnt;
+int isused1[40];
+int isused2[40];
+int isused3[40];
 
-int n,m;
-int arr[10];
-bool isused[10];
-
-
-void func(int k){ // 현재 k개까지 수를 택했음.
-  if(k == m){ // m개를 모두 택했으면
-    for(int i = 0; i < m; i++)
-      cout << arr[i] << ' '; // arr에 기록해둔 수를 출력
-    cout << '\n';
-    return;
-  }
-
-  for(int i = 1; i <= n; i++){ // 1부터 n까지의 수에 대해
-    if(!isused[i]){ // 아직 i가 사용되지 않았으면
-      arr[k] = i; // k번째 수를 i로 정함
-      isused[i] = 1; // i를 사용되었다고 표시
-      func(k+1); // 다음 수를 정하러 한 단계 더 들어감
-      isused[i] = 0; // k번째 수를 i로 정한 모든 경우에 대해 다 확인했으니 i를 이제 사용되지않았다고 명시함.
+void func(int cur){
+    if(cur == n){  //마지막 줄까지 갔다면
+        cnt++;
+        return;
     }
-  }
+    for(int i=0; i<n; i++){
+        if( isused1[i] ||  isused2[cur+i]  || isused3[cur-i + n-1]  )
+            continue;
+        isused1[i] = 1;
+        isused2[cur+i] = 1;
+        isused3[cur-i + n-1] = 1;
+        func(cur+1);
+        isused1[i] = 0;
+        isused2[cur+i] = 0;
+        isused3[cur-i + n-1] = 0;
+    }
 }
+
+
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin >> n >> m;
+  cin>>n;
   func(0);
+  cout<<cnt;
+ 
+
+
 }
 
 
