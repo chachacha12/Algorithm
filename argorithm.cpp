c342bbdb6bba74194eea464360a9b7c121afd267
chachacha12 <chaminwoo369@gmail.com>
2023-07-28 14:01:16 +0900

    백트래킹 정의

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,66 +1,7 @@
-#include <iostream>
-#include <math.h>
-#include <algorithm>
-#include <list>
-#include <stack> 
-#include <queue>
-using namespace std; 
-
-int arr[130][130];
-
-int white=0;
-int blue=0;
-
-void func(int x, int y, int size){
-
-    bool same=true;
-    for(int i=y; i<y+size; i++){
-        for(int j=x; j<x+size; j++){
-            if(arr[i][j] != arr[y][x] ){
-                same = false;
-                break;
-            }
-        }
-        if(!same)
-            break;
-    }
-    
-    if(same){
-        if(arr[y][x] ==0)
-            white++;
-        else
-            blue++;
-    }else{
-        func(x, y, size/2);
-        func(x+size/2, y, size/2);
-        func(x, y+size/2, size/2);
-        func(x+size/2, y+size/2, size/2);
-    }
-}
 
 
+/*
+백트래킹
+: 현재 상태에서 가능한 모든 후보군을 따라 들어가며 탐색하는 알고리즘
 
-int main(){ 
-    ios::sync_with_stdio(0);  
-    cin.tie(0); 
-
-    int n;
-    cin >>n;
-
-    for(int i=0; i<n; i++){
-        for(int j=0; j<n; j++){
-            cin>>arr[i][j];
-        }
-    }
-
-    func(0,0,n);
-    cout<<white<<"\n";
-    cout<<blue<<"\n";
-
-    return 0;
-}
-
-
-
-
-
+*/
