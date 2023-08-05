a347d9dddc62a625ae29385f2a818b6ef6ed11cc
chachacha12 <chaminwoo369@gmail.com>
2023-08-05 19:13:38 +0900

    15652 백트래킹 N과 M(4)

--- argorithm.cpp
+++ argorithm.cpp
@@ -10,14 +10,20 @@ using namespace std;
 int n,m;
 int a[10];
 
+
 void func(int cur){
   if(cur == m){
-    for(int i=0; i<m; i++)
-      cout << a[i] << ' ';
+    for(int i=0; i<m; i++){
+      cout<<a[i]<<' ';
+    }
     cout<<'\n';
     return;
   }
   for(int i=1; i<=n; i++){
+    if(cur !=0){
+      if(a[cur-1] > i)
+        continue;
+    }
     a[cur] = i;
     func(cur+1);
   }
@@ -27,10 +33,8 @@ void func(int cur){
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  cin >> n>>m;
+  cin>>n>>m;
   func(0);
-
 
 }
 
