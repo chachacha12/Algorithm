e301c3a36b1a1a3bdf5e542cde6b8e549706300c
chachacha12 <chaminwoo369@gmail.com>
2023-08-18 01:42:47 +0900

    6603 백트래킹 로또

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,28 +7,30 @@
 #include <queue>
 using namespace std; 
 
+//조합 문제임. 몇개중에 몇개를 고르는 문제. 
 
-int n,m;
-int a[10];
-int result[10];
+int s[15];
+int result[15];
+int k;
+bool isused[15];
 
 void func(int cur){
-  if(cur==m){
-    for(int i=0; i<m; i++){ 
-      cout<< result[i]<<' '; 
+  if(cur ==6){
+    for(int i=0; i<6; i++){
+      cout<<result[i]<<' ';
     }
     cout<<'\n';
-    return;
   }
-  int tmp=0;
-  for(int i=0; i<n; i++){
-    if(cur!=0 && result[cur-1] > a[i])
-      continue;
- 
-    if( tmp != a[i] ){ 
-      tmp = a[i];
-      result[cur] = a[i];
-      func(cur+1);
+
+  for(int i=0; i<k; i++){
+    if(!isused[i] ){
+      if(cur !=0 && result[cur-1]>= s[i] )
+          continue;
+        
+      result[cur] = s[i];
+      isused[i] = 1;
+      func(cur+1); 
+      isused[i] =0;
     }
   }
 
@@ -38,13 +40,20 @@ void func(int cur){
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  cin>>n>>m;
-  for(int i=0; i<n; i++){
-    cin>>a[i];
-  }
-  sort(a, a+n);
-  func(0);
- 
+
+  while(true){
+    cin>> k;
+    if(k==0)
+      break;
+    
+    for(int i=0; i<k; i++){
+      cin >> s[i];
+    }
+    func(0);
+     cout<<'\n';
+  }  
+
+  return 0;
 }
 
 
