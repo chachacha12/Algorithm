6d1cb9de99b8fddfb1deaabdc0ace28c8bcc6f8e
chachacha12 <chaminwoo369@gmail.com>
2023-08-26 14:53:52 +0900

    6603 백트래킹 로또 (next_permutation이용)

--- argorithm.cpp
+++ argorithm.cpp
@@ -9,49 +9,46 @@ using namespace std;
 
 //조합 문제임. 몇개중에 몇개를 고르는 문제. 
 
+
+int a[15];
 int s[15];
-int result[15];
+
+
 int k;
-bool isused[15];
-
-void func(int cur){
-  if(cur ==6){
-    for(int i=0; i<6; i++){
-      cout<<result[i]<<' ';
-    }
-    cout<<'\n';
-  }
-
-  for(int i=0; i<k; i++){
-    if(!isused[i] ){
-      if(cur !=0 && result[cur-1]>= s[i] )
-          continue;
-        
-      result[cur] = s[i];
-      isused[i] = 1;
-      func(cur+1); 
-      isused[i] =0;
-    }
-  }
-
-}
-
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   while(true){
-    cin>> k;
+    cin>>k;
     if(k==0)
       break;
-    
+
     for(int i=0; i<k; i++){
-      cin >> s[i];
+      cin>>s[i];
+
+
+      if(i>=6){
+        a[i] = 1;
+      }else{
+        a[i] = 0;
+      }
     }
-    func(0);
-     cout<<'\n';
-  }  
+
+    do{
+      for(int i=0; i<k; i++){
+        if(a[i] ==0){
+          cout<<s[i]<<" ";
+        }
+      }
+    cout<<'\n';
+    }while(next_permutation(a, a+k));
+    cout<<'\n';
+
+  }
+
+  
 
   return 0;
 }
