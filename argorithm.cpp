c0389a07f5a26cb8be840d22e28d21b47fc8d44f
chachacha12 <chaminwoo369@gmail.com>
2023-08-28 18:56:20 +0900

    1406 연결리스트 에디터

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,51 +1,63 @@
-
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-#include <list>
 #include <stack> 
 #include <queue>
+#include <list>
 using namespace std; 
 
-//조합 문제임. 몇개중에 몇개를 고르는 문제. 
+list<char> L;
+list<char>::iterator it;
 
-
-int a[15];
-int s[15];
-
-
-int k;
+string s; 
+int n;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
+  
+  cin>>s;
+  cin>>n;
 
-  while(true){
-    cin>>k;
-    if(k==0)
-      break;
+  for(auto c: s){
+    L.push_back(c);
+  }
+  it =  L.end();
+  
+  while(n--){
+    char a;
+    cin>>a;
+    if(a=='L'){
+      if(it != L.begin()){
+        it--;
+      } 
 
-    for(int i=0; i<k; i++){
-      cin>>s[i];
-
-
-      if(i>=6){
-        a[i] = 1;
-      }else{
-        a[i] = 0;
+    }else if(a=='D'){
+      if(it != L.end()){
+        it++;
       }
+
+    }else if(a=='B'){
+      if(it == L.begin() ){
+
+      }else if( it==L.end()){
+        it--;
+        it = L.erase(it);
+      }else{
+        it--;
+        it = L.erase(it);
+      }
+
+    }else{
+      char word;
+      cin>>word;
+      L.insert(it, word);
     }
 
-    do{
-      for(int i=0; i<k; i++){
-        if(a[i] ==0){
-          cout<<s[i]<<" ";
-        }
-      }
-    cout<<'\n';
-    }while(next_permutation(a, a+k));
-    cout<<'\n';
+  }
 
+  for(auto c: L){
+    cout<<c;
   }
 
   
