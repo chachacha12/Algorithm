0e5e64980b74ccf03848fbffefdbbbc5a35763a5
chachacha12 <chaminwoo369@gmail.com>
2023-08-30 14:32:04 +0900

    4949 스택의 활용 균형잡힌 세상

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,64 +1,66 @@
 #include <iostream>
 #include <math.h>
 #include <algorithm>
-#include <stack> 
+#include <stack>
 #include <queue>
 #include <list>
 using namespace std; 
 
-list<char> L;
-list<char>::iterator it;
 
-string s; 
-int n;
+
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
   
-  cin>>s;
-  cin>>n;
-
-  for(auto c: s){
-    L.push_back(c);
-  }
-  it =  L.end();
-  
-  while(n--){
-    char a;
-    cin>>a;
-    if(a=='L'){
-      if(it != L.begin()){
-        it--;
-      } 
-
-    }else if(a=='D'){
-      if(it != L.end()){
-        it++;
-      }
-
-    }else if(a=='B'){
-      if(it == L.begin() ){
-
-      }else if( it==L.end()){
-        it--;
-        it = L.erase(it);
-      }else{
-        it--;
-        it = L.erase(it);
-      }
-
-    }else{
-      char word;
-      cin>>word;
-      L.insert(it, word);
+  while(true){
+    stack<char> st;
+    string s;
+    getline(cin, s);
+    if(s=="."){
+      break;
     }
 
+    for(auto c : s){
+     
+      if(c=='('){
+        st.push(c);
+
+      }else if(c==')' ){
+        if(!st.empty() && st.top()=='('){
+          st.pop();
+        }else{
+          cout<< "no" <<'\n';
+          break;
+        }
+          
+
+      }else if(c=='['){
+        st.push(c);
+
+      }else if(c==']'){
+        if(!st.empty() && st.top()=='['){
+          st.pop();
+        }else{
+          cout<< "no" <<'\n';
+          break;
+        }
+          
+      }else if(c=='.'){
+        if(st.empty()){
+            cout<< "yes" <<'\n';
+            break;
+        }
+        else{
+          cout<< "no" <<'\n';
+          break;
+        }
+      }
+    }
+
+
   }
 
-  for(auto c: L){
-    cout<<c;
-  }
 
   
 
