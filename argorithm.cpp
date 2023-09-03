08bcf4e24fc30bf29036d44e8fab1cfcc58a75d1
chachacha12 <chaminwoo369@gmail.com>
2023-09-04 01:17:40 +0900

    1926 BFS 그림

--- argorithm.cpp
+++ argorithm.cpp
@@ -3,69 +3,84 @@
 #include <algorithm>
 #include <stack>
 #include <queue>
-#include <list>
 using namespace std; 
 
+#define X first
+#define Y second
 
+int dx[4] ={1,0,-1,0};
+int dy[4] ={0,1,0,-1};
 
+int arr[502][502];
+bool vis[502][502];
+
+int n,m;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-  
-  while(true){
-    stack<char> st;
-    string s;
-    getline(cin, s);
-    if(s=="."){
-      break;
+
+  cin>>n>>m;
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      cin>>arr[i][j];
     }
-
-    for(auto c : s){
-     
-      if(c=='('){
-        st.push(c);
-
-      }else if(c==')' ){
-        if(!st.empty() && st.top()=='('){
-          st.pop();
-        }else{
-          cout<< "no" <<'\n';
-          break;
-        }
-          
-
-      }else if(c=='['){
-        st.push(c);
-
-      }else if(c==']'){
-        if(!st.empty() && st.top()=='['){
-          st.pop();
-        }else{
-          cout<< "no" <<'\n';
-          break;
-        }
-          
-      }else if(c=='.'){
-        if(st.empty()){
-            cout<< "yes" <<'\n';
-            break;
-        }
-        else{
-          cout<< "no" <<'\n';
-          break;
-        }
-      }
-    }
-
-
   }
 
+  int mx=0; //계속 업뎃되는 가장 큰 그림의 크기
+  int num=0; //그림의 갯수
+
+  for(int i=0; i<n; i++){
+    for(int j=0; j<m; j++){
+      
+      //그림이 없는 칸이거나 방문한적 있는 칸일경우엔 패스
+      if( arr[i][j]==0 || vis[i][j]) 
+        continue;
+      
+      
+        queue<pair<int,int>> q;
+        num++;
+
+
+        int size=0;
+        q.push({i,j});
+        vis[i][j] = 1;
+        
+        while(!q.empty()){  
+          size++;
+          auto cur = q.front();
+          q.pop(); 
+
+          for(int i=0; i<4; i++){
+            int x = cur.X + dx[i];
+            int y = cur.Y + dy[i];
+
+            if(x<0 || x >=n || y<0 || y>=m)
+              continue;
+
+            if(arr[x][y]!=1 || vis[x][y] ==1)
+              continue;
+
+          
+            vis[x][y] =1;
+            q.push({x,y});
+          }
+
+        }
+      mx = max(mx, size); //mx에 최댓값 저장
+    }
+  }
+
+  cout<<num<<'\n';
+  cout<<mx;
+ 
 
   
 
   return 0;
 }
+
 
 
 
