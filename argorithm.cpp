0be2b2b2e440760cdefd0156e913faff79246b2a
chachacha12 <chaminwoo369@gmail.com>
2023-09-08 13:55:42 +0900

    2178 BFS 미로탐색

--- argorithm.cpp
+++ argorithm.cpp
@@ -9,77 +9,88 @@ using namespace std;
 #define Y second
 
 int dx[4] ={1,0,-1,0};
-int dy[4] ={0,1,0,-1};
+int dy[4] = {0,1,0,-1};
 
-int arr[502][502];
-bool vis[502][502];
+
+string board[102];
+int dist[102][102];
+
+queue<pair<int,int>> q;
 
 int n,m;
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
-
-  cin>>n>>m;
-
-  for(int i=0; i<n; i++){
-    for(int j=0; j<m; j++){
-      cin>>arr[i][j];
-    }
-  }
-
-  int mx=0; //계속 업뎃되는 가장 큰 그림의 크기
-  int num=0; //그림의 갯수
-
-  for(int i=0; i<n; i++){
-    for(int j=0; j<m; j++){
-      
-      //그림이 없는 칸이거나 방문한적 있는 칸일경우엔 패스
-      if( arr[i][j]==0 || vis[i][j]) 
-        continue;
-      
-      
-        queue<pair<int,int>> q;
-        num++;
-
-
-        int size=0;
-        q.push({i,j});
-        vis[i][j] = 1;
-        
-        while(!q.empty()){  
-          size++;
-          auto cur = q.front();
-          q.pop(); 
-
-          for(int i=0; i<4; i++){
-            int x = cur.X + dx[i];
-            int y = cur.Y + dy[i];
-
-            if(x<0 || x >=n || y<0 || y>=m)
-              continue;
-
-            if(arr[x][y]!=1 || vis[x][y] ==1)
-              continue;
-
-          
-            vis[x][y] =1;
-            q.push({x,y});
-          }
-
-        }
-      mx = max(mx, size); //mx에 최댓값 저장
-    }
-  }
-
-  cout<<num<<'\n';
-  cout<<mx;
- 
-
   
+  cin >> n >> m;
+  
+  for(int i=0; i<n; i++){
+    cin>>board[i];
+  }
+
+  q.push({0,0});
+  dist[0][0] = 1;
+  int end =0;
+  
+  while(!q.empty()){
+    auto cur = q.front();
+    q.pop();
+
+    for(int i=0; i<4; i++){
+      int x = cur.X + dx[i];
+      int y = cur.Y + dy[i];
+      
+      if(x<0|| x>=n || y<0 || y>=m)
+        continue;
+
+      if(board[x][y] != '1' || dist[x][y] !=0   )
+        continue;
+
+      q.push({x,y});
+      dist[x][y] = dist[cur.X][cur.Y]+1;
+      
+    }
+  }
+
+  cout<<dist[n-1][m-1];
+
 
   return 0;
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
