36417ae5325314d4fe1ac0c77bc22ec4fdf696cb
chachacha12 <chaminwoo369@gmail.com>
2023-10-10 03:15:52 +0900

    15650 백트래킹 N과 M(2)

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,41 +6,42 @@
 using namespace std; 
 
 
+
 int arr[10];
 bool isused[10];
 int n,m;
 
 
-void func(int k){
-    if(k==m){
-      for(int i=0; i<m; i++){
-        cout<<arr[i]<<' ';
-      }
-      cout<<'\n';
-      return;
+void func(int cur){
+  if(cur==m){
+    for(int i=0; i<m; i++){
+      cout<<arr[i]<<' ';
     }
-    for(int i=1; i<=n; i++){
-      if(!isused[i]){
-        arr[k]=i;
-        isused[i]=1;
-        func(k+1);
-        isused[i]=0;
-      }
+    cout<<'\n';
+    return;
+  }
+  for(int i=1; i<=n; i++){
+      if( cur!=0 && arr[cur-1] > i  )
+          continue;
+    
+    if(!isused[i] ){
+         isused[i] =1;
+         arr[cur] = i;
+         func(cur+1);
+         isused[i]=0;
     }
+  }
 }
-
-
-
 
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  cin >> n >> m;
+  cin>>n>>m;
   func(0);
+
   
- 
 
   return 0;
 }
