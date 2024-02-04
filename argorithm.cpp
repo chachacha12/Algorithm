8276f421a5c413561b8bbc9ac01f2be5f10208e8
chachacha12 <chaminwoo369@gmail.com>
2024-02-04 18:26:05 +0900

    11652 정렬 카드

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,19 +1,52 @@
-///////////////////////////////////////////
-카운팅 소트
-
-n이 1000만이하의 작은 수라면, 카운팅 소트 쓰면 구현쉽고 빠름.
-나오는 숫자의 갯수를 세어두었다가 출력해주면 됨. 하지만 수가 크면 이거 못씀.
-
-
-///////////////////////////////////////////
-Radix 소트
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std; 
 
 
-///////////////////////////////////////////
-  STL 소트 -> 코테는 이거 쓰면됨. O(nlogn) 걸림.
+long long arr[100002];
 
-  int a[5] = {1,4,5,2,7};
-  sort(a,a+5);
+int main(void){
+  ios::sync_with_stdio(0);
+  cin.tie(0);
 
-  vector<int> b = {1,4,5,2,7};
-  sort(b.begin(), b.end());
+  int n;
+  cin>>n;
+
+  for(int i=0; i<n; i++){
+    cin>>arr[i];
+  }
+
+  sort(arr, arr+n);
+
+  int cnt=0;
+  int maxcnt=0;
+  long long maxval= -pow(2,62)-1; 
+
+  for(int i=0; i<n; i++){
+    if(i==0 || arr[i-1] == arr[i]){
+      cnt++;
+    }else{
+      if(maxcnt < cnt){
+        maxcnt = cnt;
+        maxval = arr[i-1];
+      }
+      cnt=1; 
+    }
+  }
+
+  if(cnt > maxcnt){
+    maxval = arr[n-1];
+  }
+
+
+  cout<< maxval;
+
+
+
+  return 0;
+}
+
