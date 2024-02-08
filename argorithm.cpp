38dae56482a2e05194ad7224b2ec15fe0d4d57e1
chachacha12 <chaminwoo369@gmail.com>
2024-02-08 15:31:56 +0900

    2751 정렬 수 정렬하기2

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,43 +7,24 @@
 using namespace std; 
 
 
-long long arr[100002];
-
+int n;
+int arr[1000002];
+  
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
-  int n;
   cin>>n;
 
   for(int i=0; i<n; i++){
     cin>>arr[i];
   }
-
+  
   sort(arr, arr+n);
 
-  int cnt=0;
-  int maxcnt=0;
-  long long maxval= -pow(2,62)-1; 
-
   for(int i=0; i<n; i++){
-    if(i==0 || arr[i-1] == arr[i]){
-      cnt++;
-    }else{
-      if(maxcnt < cnt){
-        maxcnt = cnt;
-        maxval = arr[i-1];
-      }
-      cnt=1; 
-    }
+    cout<<arr[i]<<'\n';
   }
-
-  if(cnt > maxcnt){
-    maxval = arr[n-1];
-  }
-
-
-  cout<< maxval;
 
 
 
