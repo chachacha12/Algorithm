50dc5fad618fabfc2aea918c2b8882dab0a0ef87
chachacha12 <chaminwoo369@gmail.com>
2024-02-11 04:32:54 +0900

    11931 정렬 수 정렬하기4

--- argorithm.cpp
+++ argorithm.cpp
@@ -8,28 +8,24 @@ using namespace std;
 
 
 int n;
-int arr[10002];
+int arr[1000002];
   
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin>>n;
-
+  
   for(int i=0; i<n; i++){
-    int a;
-    cin>>a;
-    arr[a]++;
+    cin>>arr[i];
   }
 
-  //i가 n개까지 확인하는게 아니라 10002까지만 확인해야함 꼭
-  for(int i=0; i<10002; i++){
-   if(arr[i]!=0){
-      while(arr[i]--){
-        cout<<i<<"\n";
-      }
-   }
+  sort(arr,arr+n);
+  
+  for(int i=n-1; i>=0; i--){
+    cout<<arr[i]<<'\n';
   }
+
 
 
   return 0;
