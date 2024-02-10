7128737e6cf1ee1b03bdc17aaa676e3dde10886a
chachacha12 <chaminwoo369@gmail.com>
2024-02-11 05:16:51 +0900

    10814 정렬 나이순 정렬

--- argorithm.cpp
+++ argorithm.cpp
@@ -8,25 +8,31 @@ using namespace std;
 
 
 int n;
-int arr[1000002];
+pair<int, string> arr[100002];
+
+
+//사용자정의 함수 - sort()함수에 넣으면 이거대로 정렬가능
+bool compare(pair<int, string> a, pair<int, string> b){
+  return a.first < b.first;  
+}
   
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0);
 
   cin>>n;
-  
+
   for(int i=0; i<n; i++){
-    cin>>arr[i];
+    cin>>arr[i].first>>arr[i].second;
   }
 
-  sort(arr,arr+n);
+  stable_sort(arr, arr+n, compare);
+
+
+  for(int i=0; i<n; i++){
+    cout<< arr[i].first <<" "<< arr[i].second<<'\n';
+  }
   
-  for(int i=n-1; i>=0; i--){
-    cout<<arr[i]<<'\n';
-  }
-
-
 
   return 0;
 }
