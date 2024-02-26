a391b0706daad118b14d2f395f2b2d4396c7b3a6
chachacha12 <chaminwoo369@gmail.com>
2024-02-26 18:42:09 +0900

    2579 dp 계단오르기

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,40 +7,35 @@
 using namespace std; 
 
 
-int d[15];
-int t;
+int d[305][3];
+int s[305];
 
 
 int main(void){
   ios::sync_with_stdio(0);
-  cin.tie(0);
+  cin.tie(0); 
 
-  //점화식 
-  //  d[i] = d[i-1]    +  d[i-2]  + d[i-3]
+  //점화식
+  // d[k][1] = max(d[k-2][1], d[k-2][2]) + s[k]
+  // d[k][2] = d[k-1][1] + s[k]
 
-  
-  cin >>t;
   int n;
+  cin>>n;
 
-    d[1] = 1;
-    d[2] = 2;
-    d[3] = 4;
+   for(int i=1; i<=n; i++){
+    cin>>s[i];
+   }
 
-  while(t--){
-    cin>>n;
-  
-    if(n>=4){
-      for(int i=4; i<=n; i++){
-        d[i] = d[i-1] + d[i-2] +d[i-3];
-      }
-      cout<<d[n]<<'\n';
-    }else{
-      cout<<d[n]<<'\n';
-    }
-  
+  d[1][1] = s[1]; d[1][2] = 0;
+  d[2][1] = s[2]; d[2][2] = s[1]+s[2];
+
+
+  for(int i=3; i<=n; i++){
+    d[i][1] = max(d[i-2][1], d[i-2][2]) + s[i];
+    d[i][2] = d[i-1][1] + s[i]; 
   }
 
- 
+  cout << max(d[n][1], d[n][2]);
   
   return 0;
 }
