df2cb328dbc62d88d36a55c052868182f633509d
chachacha12 <chaminwoo369@gmail.com>
2024-02-29 00:51:39 +0900

    11726 dp 2xn 타일링

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,54 +6,39 @@
 #include <vector>
 using namespace std; 
 
+
+int d[1005];
 int n;
-int R[1005];
-int G[1005];
-int B[1005];
-int d[1005][3];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
-  /*
+ /*
+ 테이블 정의: 
+ d[i] = 2 x i 크기의 직사각형을 채우는 방법의 수
 
-  테이블 정의: 
-  d[i][0] = i번째 집을 빨강으로 칠했을때 비용의 최솟값
-  d[i][1] = i번째 집을 초록으로 칠했을때 비용의 최솟값
-  d[i][2] = i번째 집을 파랑으로 칠했을때 비용의 최솟값
+ 점화식:
+ d[k] = d[k-1] + d[k-2]
 
-  점화식:
-  d[k][0] = min(d[k-1][1], d[k-1][2]) + R[k]
-  d[k][1] = min(d[k-1][0], d[k-1][2]) + G[k]
-  d[k][2] = min(d[k-1][1], d[k-1][0]) + B[k]
+ 초기값:
+ d[1] = 1
+ d[2] = 2
+ 
+ */
 
-  초기값: 
-  d[1][0] = R[1]
-  d[1][1] = G[1]
-  d[1][2] = B[1]
-  */
+cin>>n;
 
-  cin>>n;
+d[1] = 1;
+d[2] = 2;
 
-  for(int i=1; i<=n; i++){
-    cin>>R[i] >>G[i]>> B[i];
-  }
+for(int i=3; i<=n; i++){
+  d[i] = (d[i-1] + d[i-2])%10007;
+}
 
-  d[1][0] = R[1];
-  d[1][1] = G[1];
-  d[1][2] = B[1];
-
-  for(int k=2; k<=n; k++){
-    d[k][0] = min(d[k-1][1], d[k-1][2]) + R[k];
-    d[k][1] = min(d[k-1][0], d[k-1][2]) + G[k];
-    d[k][2] = min(d[k-1][1], d[k-1][0]) + B[k];
-  }
+cout<<d[n];
 
 
-  cout<< min({d[n][0], d[n][1], d[n][2]});
-
-  
   return 0;
 }
 
