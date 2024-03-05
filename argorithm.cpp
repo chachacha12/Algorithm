138b7743119e89a2f52c9cda984e6cd4cc9021ae
chachacha12 <chaminwoo369@gmail.com>
2024-03-05 19:32:40 +0900

    11659 dp 구간 합 구하기4

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,43 +6,48 @@
 #include <vector>
 using namespace std; 
 
+int n,m;
 
-int d[1005];
-int n;
+int a[100005];
+int d[100005];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
- /*
- 테이블 정의:
- d[i] = 2*i 크기의 직사각형을 채우는 방법의 수
 
- 점화식:
- d[k] = d[k-1] +d[k-2]
+  /*
+  테이블 정의:
+    d[i] = a[1]+a[2]+ .. +a[i]
+    d[i] = d[i-1] +a[i] 
+
+  점화식:
+    d[i] = d[i-1] +a[i]
+
+    i부터 j까지의 합 구하는 점화식:
+
+    result = d[j]-d[i-1]
+
+
+  초기값:
+  d[0]
   
+  */
 
- 초기값:
- d[1] = 1
- d[2] = 2
- d[3] = 3
- 
- */
+ d[0] = 0; 
 
-  cin>>n;
+ cin>>n>>m;
+ for(int i=1; i<=n; i++){
+  cin>>a[i];
 
-  d[1] = 1;
-  d[2] = 2;
+  d[i] = d[i-1]+a[i];
+ }
 
-  for(int i=3; i<=n; i++){
-    int sum = d[i-1]+d[i-2];
-    d[i] = sum%10007;
-  }
-
-  cout<<d[n];
-
-
-
+ int i,j;
+ while(m--){
+  cin>>i>>j;
+  cout<<d[j]-d[i-1]<<'\n';
+ }
 
 
   return 0;
