6efa30ebc1de93a87a858ed4e991ea49060193fd
chachacha12 <chaminwoo369@gmail.com>
2024-03-06 14:34:35 +0900

    1463 dp 1로 만들기

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,10 +6,8 @@
 #include <vector>
 using namespace std; 
 
-int n,m;
-
-int a[100005];
-int d[100005];
+int n;
+int d[1000005];
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -18,36 +16,30 @@ int main(void){
 
   /*
   테이블 정의:
-    d[i] = a[1]+a[2]+ .. +a[i]
-    d[i] = d[i-1] +a[i] 
-
+  d[i] = i는 연산을 사용하는 횟수의 최솟값 
+  
   점화식:
-    d[i] = d[i-1] +a[i]
-
-    i부터 j까지의 합 구하는 점화식:
-
-    result = d[j]-d[i-1]
+  d[k] = d[k-1]+1, d[k/2]+1, d[k/3]+1
 
 
   초기값:
-  d[0]
   
   */
+  cin>>n;
+  d[1] = 0;
 
- d[0] = 0; 
+  for(int i=2; i<=n; i++){
+    d[i] = d[i-1]+1;
+    if(i%2==0) d[i] = min(d[i], d[i/2]+1);
+    if(i%3==0) d[i] = min(d[i], d[i/3]+1);
+  }
 
- cin>>n>>m;
- for(int i=1; i<=n; i++){
-  cin>>a[i];
+  cout<<d[n];
 
-  d[i] = d[i-1]+a[i];
- }
 
- int i,j;
- while(m--){
-  cin>>i>>j;
-  cout<<d[j]-d[i-1]<<'\n';
- }
+
+
+ 
 
 
   return 0;
