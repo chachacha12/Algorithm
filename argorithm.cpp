0727d83bc870f216c8b7de3efa0cd614527d75fe
chachacha12 <chaminwoo369@gmail.com>
2024-03-06 16:20:38 +0900

    1003 dp 피보나치 함수

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,9 +6,12 @@
 #include <vector>
 using namespace std; 
 
+
+
 int n;
-int d[1000005];
-int pre[1000005];
+int t;
+int d[42][1];
+
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -17,37 +20,54 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i] = i는 연산을 사용하는 횟수의 최솟값 
+  d[i][0] = i일때 0이 출력되는 횟수
+  d[i][1] = i일때 1이 출력되는 횟수 
   
+  d[0][0] = 1
+  d[1][0] = 0
+  d[2][0] = 1
+  d[3][0] = 1
+  d[4]    = 2
+  d[5]    = 3
+  d[6]    = 5
+
+  d[0][1] = 0
+  d[1][1] = 1
+  d[2][1] = 1
+  d[3][1] = 2
+  d[4]    = 3
+  d[5]    = 5
+  d[6]    = 8
+
+
   점화식:
-  d[k] = d[k-1]+1, d[k/2]+1, d[k/3]+1 중 최솟값
-  
+  d[k][0] = d[k-1][0] + d[k-2][0] 
+  d[k][1] = d[k-1][1] + d[k-2][1]
+
   초기값:
+
   */
+
+ cin>>t;
+
+ d[0][0] = 1;
+ d[1][0] = 0;
+
+ d[0][1] = 0;
+ d[1][1] = 1;
+
+ while(t--){
   cin>>n;
-  d[1] = 0;
-
+  
   for(int i=2; i<=n; i++){
-    d[i] = d[i-1]+1;
-    pre[i] = i-1;
-    if(i%2 == 0 && d[i] > d[i/2] + 1){
-      d[i] = d[i/2] + 1;
-      pre[i] = i/2;
-    }
-    if(i%3 == 0 && d[i] > d[i/3] + 1){
-      d[i] = d[i/3] + 1;
-      pre[i] = i/3;
-    }
+    d[i][0] = d[i-1][0] + d[i-2][0];
+    d[i][1] = d[i-1][1] + d[i-2][1];
   }
 
-  cout<<d[n]<<'\n';
+  cout<<d[n][0]<<" "<<d[n][1]<<"\n";
 
-  int cur = n;
-  while(true){
-    cout<<cur<<" ";
-    if(cur==1) break;
-    cur = pre[cur];
-  }
+ }
+
  
   return 0;
 }
