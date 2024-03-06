40cb1681eb2267ccb00caf57a7c657e60bd113d5
chachacha12 <chaminwoo369@gmail.com>
2024-03-06 15:30:38 +0900

    1로 만들기 2

--- argorithm.cpp
+++ argorithm.cpp
@@ -8,6 +8,7 @@ using namespace std;
 
 int n;
 int d[1000005];
+int pre[1000005];
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -19,29 +20,35 @@ int main(void){
   d[i] = i는 연산을 사용하는 횟수의 최솟값 
   
   점화식:
-  d[k] = d[k-1]+1, d[k/2]+1, d[k/3]+1
-
-
-  초기값:
+  d[k] = d[k-1]+1, d[k/2]+1, d[k/3]+1 중 최솟값
   
+  초기값:
   */
   cin>>n;
   d[1] = 0;
 
   for(int i=2; i<=n; i++){
     d[i] = d[i-1]+1;
-    if(i%2==0) d[i] = min(d[i], d[i/2]+1);
-    if(i%3==0) d[i] = min(d[i], d[i/3]+1);
+    pre[i] = i-1;
+    if(i%2 == 0 && d[i] > d[i/2] + 1){
+      d[i] = d[i/2] + 1;
+      pre[i] = i/2;
+    }
+    if(i%3 == 0 && d[i] > d[i/3] + 1){
+      d[i] = d[i/3] + 1;
+      pre[i] = i/3;
+    }
   }
 
-  cout<<d[n];
+  cout<<d[n]<<'\n';
 
-
-
-
+  int cur = n;
+  while(true){
+    cout<<cur<<" ";
+    if(cur==1) break;
+    cur = pre[cur];
+  }
  
-
-
   return 0;
 }
 
