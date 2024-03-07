f4654dea5643b8500b8aa4ec42480a15f5ffbb54
chachacha12 <chaminwoo369@gmail.com>
2024-03-07 16:45:20 +0900

    11727 dp 2xn 타일링 2

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,8 +6,8 @@
 #include <vector>
 using namespace std; 
 
-int d[505][505];
-int a[505][505];
+
+int d[1005];
 int n;
 
 int main(void){
@@ -17,39 +17,22 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i][j] = i층 j칸까지일때 최댓값
-
+  d[i] = 2*i직사각형을 3개 타일로 채우는 방법의 수
 
   점화식:
-  d[i][j] =  max(d[i-1][j-1], d[i-1][j]) + a[i][j]
-
-  초기값:
-
+  d[k] = d[k-1] + d[k-2] + d[k-2]
   */
 
- cin >> n;
+  cin >> n;
 
- for(int i=1; i<=n; i++){
-  for(int j=1; j<=i; j++){
-    cin >> a[i][j];
+  d[1] = 1;
+  d[2] = 3;
+
+  for(int i=3; i<=n; i++){
+     d[i] = (d[i-1] + d[i-2] + d[i-2]) % 10007  ;
   }
- }
-
-d[1][1] = a[1][1];
-
-int result=d[1][1];
-
-for(int i=2; i<=n; i++){
-  for(int j=1; j<=i; j++){
-     d[i][j] = max(d[i-1][j-1], d[i-1][j]) + a[i][j];
-     result = max(d[i][j], result);
-  }
-}
-
-// cout << *max_element(d[n] + 1, d[n] + n+1);  //이러면 1번부터 n번까지 중 최댓값 가져옴
-cout<<result;
-
-
+  
+  cout<<d[n];
  
 
  
