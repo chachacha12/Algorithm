59e05676a12c8cd3cf8eaada188da6755c536c7c
chachacha12 <chaminwoo369@gmail.com>
2024-03-07 17:02:43 +0900

    2193 dp 이친수

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,8 +7,8 @@
 using namespace std; 
 
 
-int d[1005];
 int n;
+long long d[100];
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -17,23 +17,32 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i] = 2*i직사각형을 3개 타일로 채우는 방법의 수
+  d[i] = i자리 이친수의 개수
+
+10000
+
+  d[1] = 1
+  d[2] = 1
+  d[3] = 2
+  d[4] = 3
+  d[5] = 5
 
   점화식:
-  d[k] = d[k-1] + d[k-2] + d[k-2]
+  d[k] = d[k-1] + d[k-2]
+
   */
 
-  cin >> n;
+ cin>>n;
 
   d[1] = 1;
-  d[2] = 3;
+  d[2] = 1;
 
   for(int i=3; i<=n; i++){
-     d[i] = (d[i-1] + d[i-2] + d[i-2]) % 10007  ;
+    d[i] = d[i-1] + d[i-2];
   }
-  
+
   cout<<d[n];
- 
+
 
  
 
