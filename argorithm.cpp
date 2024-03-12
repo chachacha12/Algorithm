2ced3957fcc33b1311a12071a753fb068bec4b0a
chachacha12 <chaminwoo369@gmail.com>
2024-03-12 19:10:20 +0900

    9461 dp 파도반 수열

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,43 +6,37 @@
 #include <vector>
 using namespace std; 
 
-
-int n;
-int d[100005];
-int a[100005];
+int t,n;
+long long d[105];
 
 int main(void){
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
-
   /*
   테이블 정의:
-  d[i] = i번째 까지 봤을때 구할 수 있는 최댓값
-
-
-  점화식:
-  d[k] = max( d[k-1] +a[k], a[k] )  
+  d[i] = i번째 정삼각형의 변의 길이
   
+  점화식:
+  d[k] = d[k-1]+d[k-5] 
 
+  
   */
 
- cin>>n;
- for(int i=1; i<=n; i++){
-  cin>>a[i];
+ cin>>t;
+
+ d[1] =1, d[2]=1, d[3]=1, d[4]=2, d[5]=2;
+
+ for(int i=6; i<=101; i++){
+  d[i] = d[i-1]+d[i-5];
  }
 
-d[1] = a[1];
+while(t--){
+  cin>>n;
+  cout<<d[n]<<'\n';
+}
 
-
-int result = a[1];
-
- for(int i=2; i<=n; i++){
-   d[i] = max( d[i-1] +a[i], a[i] ) ;
-   if(result<d[i]) result = d[i];
- }
-
- cout<<result;
+ 
 
 
   return 0;
