489e86454603ce7e38c220a6d899470371180ff2
chachacha12 <chaminwoo369@gmail.com>
2024-03-12 20:49:28 +0900

    14501 dp 퇴사(삼성 sw 역량 테스트 기출1)

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,8 +6,11 @@
 #include <vector>
 using namespace std; 
 
-int t,n;
-long long d[105];
+int n;
+int t[20];
+int p[20];
+int d[20];
+
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -15,28 +18,39 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i] = i번째 정삼각형의 변의 길이
-  
-  점화식:
-  d[k] = d[k-1]+d[k-5] 
+  d[i] = i일까지 일했을때 얻을 수 있는 최대 수익 (뒤부터)
 
-  
+  점화식:
+
+  if(n + tn <= n+1 ) //해당 상담을 퇴사 전에 할 수 있을때
+
+  d[n] =  max(d[n+tn]+pn, d[n+1]  ) //상담 하기로 할때와 안하기로 할때 중 최댓값을 저장 
+
+  else  //해당 상담을 선택여지없이 못할때
+  d[n] = d[n+1]
+
   */
 
- cin>>t;
-
- d[1] =1, d[2]=1, d[3]=1, d[4]=2, d[5]=2;
-
- for(int i=6; i<=101; i++){
-  d[i] = d[i-1]+d[i-5];
- }
-
-while(t--){
   cin>>n;
-  cout<<d[n]<<'\n';
-}
 
- 
+  for(int i=1; i<=n; i++){
+    cin>>t[i]>>p[i];
+  }
+
+  d[n+1] = 0;
+
+  for(int i=n; i>=1; i--){
+     if(i + t[i] <= n+1 ){//해당 상담을 퇴사 전에 할 수 있을때
+      d[i] =  max(d[i+t[i]]+p[i], d[i+1]  );
+      //cout<<"d["<<i<<"]: "<<d[i];
+     }else{
+      d[i] =d[i+1];
+      //cout<<"d["<<i<<"]: "<<d[i];
+     }
+  }
+
+  cout<<d[1];
+
 
 
   return 0;
