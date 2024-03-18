4e630d717188c297561b94cf3bfcd4b74fa0c1f3
chachacha12 <chaminwoo369@gmail.com>
2024-03-18 22:31:13 +0900

    14501 dp 퇴사

--- argorithm.cpp
+++ argorithm.cpp
@@ -7,10 +7,9 @@
 using namespace std; 
 
 int n;
-int t[20];
-int p[20];
-int d[20];
-
+int t[17];
+int p[17];
+int d[17];
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -18,38 +17,37 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i] = i일까지 일했을때 얻을 수 있는 최대 수익 (뒤부터)
+  d[i] = i일까지 했을때 얻을 수 있는 최대 이익(뒤부터)
 
   점화식:
+  
+  if(n+tn <= n+1)  //범위에 들어서 상담을 할지 말지 정할 수 있는 경우
+  d[i] = max( pi+d[d[i+ti]], d[i+1] )
 
-  if(n + tn <= n+1 ) //해당 상담을 퇴사 전에 할 수 있을때
+  
+  -시간오버로 아예 상담을 시도조차 못할경우
+  d[i] = d[i+1]
 
-  d[n] =  max(d[n+tn]+pn, d[n+1]  ) //상담 하기로 할때와 안하기로 할때 중 최댓값을 저장 
-
-  else  //해당 상담을 선택여지없이 못할때
-  d[n] = d[n+1]
 
   */
 
-  cin>>n;
+ cin>>n;
 
-  for(int i=1; i<=n; i++){
-    cin>>t[i]>>p[i];
-  }
+ for(int i=1; i<=n; i++){
+  cin>>t[i]>>p[i];
+ }
 
-  d[n+1] = 0;
+ for(int i=n; i>=1; i--){
+   if(i+t[i] <= n+1) {
+      d[i] = max( p[i]+d[i+t[i]], d[i+1]);
+   }else{
+      d[i] = d[i+1];
+   }
+ }
 
-  for(int i=n; i>=1; i--){
-     if(i + t[i] <= n+1 ){//해당 상담을 퇴사 전에 할 수 있을때
-      d[i] =  max(d[i+t[i]]+p[i], d[i+1]  );
-      //cout<<"d["<<i<<"]: "<<d[i];
-     }else{
-      d[i] =d[i+1];
-      //cout<<"d["<<i<<"]: "<<d[i];
-     }
-  }
+ cout<<d[1];
+  
 
-  cout<<d[1];
 
 
 
