eaefc14bd7d820e8ffbb59cdeeb080fd19e778c2
chachacha12 <chaminwoo369@gmail.com>
2024-03-20 18:58:52 +0900

    9655 dp 돌게임

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,9 +6,7 @@
 #include <vector>
 using namespace std; 
 
-int t,n,m;
-int a[25];
-int d[10005];
+int n;
 
 int main(void){
   ios::sync_with_stdio(0);
@@ -16,47 +14,19 @@ int main(void){
 
   /*
   테이블 정의:
-  d[i] = i원 만드는 방법의 수
 
 
   점화식:
-  d[i] = d[i]+d[i-a[i]]
-
+ 
   */
 
- cin>>t;
+ cin>>n;
 
- while(t--){
-  fill(d, d+10005,0);
-  cin>>n;
-
-    //동전 종류들 저장
-  for(int i=0; i<n; i++ ){
-    cin>>a[i];
-  }
-
-  cin>>m;
-
-  d[0] = 1;
-
-  for(int i=0; i<n; i++){   //동전갯수만큼 반복. 즉 새로운 동전이 추가될때마다 d[i]값은 갱신해줌
-    for(int j=a[i]; j<=m; j++){   
-      d[j] = d[j] + d[j-a[i]];  
-    }
-  }
-
-  cout<<d[m]<<"\n";
-
-
- }
-
-
-
-
- 
-
-
-
+ if(n%2==0){
+  cout<<"CY";
+ }else{
+  cout<<"SK";
+ } 
 
 
   return 0;
