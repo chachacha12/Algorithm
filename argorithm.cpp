66d52afd588930ec20a002d8d185593117305d05
chachacha12 <chaminwoo369@gmail.com>
2024-04-02 16:43:42 +0900

    2217 그리디 로프

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,44 +6,37 @@
 #include <vector>
 using namespace std; 
 
-
 int n;
-pair<long long, long long> p[100005];
+vector<int> arr(100005);
 
-bool compare(pair<long long, long long> a,pair<long long, long long> b){
-  if(a.second == b.second){
-    return a.first <  b.first;
-  }
-  return a.second < b.second;
+bool compare(int a, int b){
+  return a>b;
 }
-
-
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
-  
+
   cin>>n;
   
   for(int i=0; i<n; i++){
-    cin >> p[i].first>>p[i].second;
+    int a;
+    cin >> a;
+    arr.push_back(a);
   }
 
-  sort(p, p+n, compare);
-  
-  int ans=1;
-  long long t = p[0].second;
+  sort(arr.begin(), arr.end(), compare);
 
-  for(int i=1; i<n; i++){
-    
-   if(p[i].first >= t ){
-        ans++;
-        t = p[i].second;
-    }
+
+  int maxvalue=arr[0];
+  int result;
+
+  for(int i=0; i<n; i++){
+    result = arr[i] * (i+1) ; 
+    maxvalue = max(maxvalue, result); 
   }
-  cout<<ans;
 
-
+  cout<<maxvalue;
   
   return 0;
 }
