38ea0dc24f6cadaa8c6107ab4a2508b21680c3e6
chachacha12 <chaminwoo369@gmail.com>
2024-04-11 18:38:09 +0900

    1744 그리디 수묶기

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,37 +6,72 @@
 #include <vector>
 using namespace std; 
 
+int n;
+vector<int> p;
+vector<int> m;
 
-int t, n;
-int a[1000005];
+long long sum;
+
+bool compare(int a, int b){
+  return a>b;
+}
+
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
-  cin>>t;
+  cin>>n;
 
-  while(t--){
-
-    cin>>n;
-
-    for(int i=0; i<n; i++){
-      cin>>a[i];
-    }
-    
-    int mx = a[n-1];
-    long long sum=0;
-
-    for(int i=n-2; i>=0; i--){  
-      if(a[i] < mx){  
-        sum = sum + (mx-a[i]);
-      }else{
-        mx = a[i];
-      }
-    }
-    cout<<sum<<'\n';
+  for(int i=0; i<n; i++){
+    int num;
+    cin>>num;
+    if(num>0)
+      p.push_back(num);
+    else
+      m.push_back(num);
   }
   
+  sort(p.begin(), p.end(), compare); //내림차순
+  sort(m.begin(), m.end()); //오름차순
+
+  //양수 처리
+  for(int i=0; i<p.size(); i+=2){
+    
+    while(p.back()==1){
+      sum+=1;
+      p.pop_back();
+      //cout<<"1sum: "<<sum<<'\n';
+    }
+
+    if(p.size()%2!=0){  //홀수면
+      sum+= p.back();
+      p.pop_back();
+      //cout<<"2sum: "<<sum<<'\n';
+    }
+    if(p.size()!=0){
+
+      sum+=p[i]*p[i+1];
+    }
+  
+    //cout<<"3sum: "<<sum<<'\n';
+  } 
+
+  //음수처리
+  for(int i=0; i<m.size(); i+=2){
+    
+    if(m.size()%2!=0){  //홀수면
+      sum+= m.back();
+      m.pop_back();
+    }
+
+    sum+=m[i]*m[i+1];
+    //cout<<"sum: "<<sum<<'\n';
+  } 
+
+
+  cout<<sum;
+
   
   return 0;
 }
