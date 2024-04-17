27fa6c7fc6de2226aade0a4933e6a7fd26562082
chachacha12 <chaminwoo369@gmail.com>
2024-04-17 17:36:17 +0900

    18870 이분탐색 좌표압축

--- argorithm.cpp
+++ argorithm.cpp
@@ -8,16 +8,34 @@ using namespace std;
 
 
 
-int n,m;
-int a[500005];
+int n;
+
+int input[1000005];
+int a[1000005];
+int b[1000005];
+int len;
 
 
 int check(int target){
+  int st=0; 
+  int en = len;
+  int mid=0;
 
-  int* low = lower_bound(a, a+n, target);  //정렬된 배열에서 target값이 정렬 유지하면서 들어갈 수 있는 곳의 가장 아래 주소값
-  int* upper = upper_bound(a, a+n, target); //정렬된 배열에서 target값이 정렬 유지하면서 들어갈 수 있는 곳의 가장 윗 주소값
+  while(1){
+    mid = (st+en)/2;
 
-  return upper-low; //즉 배열에서 target값이 나온 횟수와 같음
+    if(b[mid]<target){
+      st = mid+1;
+
+    }else if(b[mid] > target){
+      en = mid-1;
+
+    }else{
+      break;
+    }
+  }
+  return mid;
+
 }
 
 
@@ -27,21 +45,32 @@ int main(void){
 
   cin>>n;
 
-  for(int i=0; i<n; i++){
+  for(int i=0; i<n ; i++){
     cin>>a[i];
+    input[i] = a[i];
+  }
+
+  sort(a, a+n);
+  
+  //중복제거 후 b배열로 옮김
+  int minus=0;
+  for(int i=0; i<n ; i++){
+    if(i == n-1 || a[i] != a[i+1]){
+      b[i-minus] = a[i];
+      len++;
+    }else{
+      minus++;
+    } 
+  }
+
+  //이제 이분탐색해서 target의 인덱스값 알아내면됨
+  for(int i=0; i<n; i++){
+
+   //cout<<"input[i]: "<<input[i]<<"\n";
+    
+    cout<<check(input[i])<<" ";
   }
   
-  sort(a, a+n);
-
-  int m;
-  cin>>m;
-
-  int t;
-  while(m--){
-    cin>>t;
-
-    cout<<check(t)<<" ";
-  }
 
   return 0;
 }
