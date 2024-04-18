b092ed05092328b713763d9deb2e8bb81d809576
chachacha12 <chaminwoo369@gmail.com>
2024-04-18 18:03:21 +0900

    19970 이분탐색 좌표압축(Unique함수이용)

--- argorithm.cpp
+++ argorithm.cpp
@@ -9,34 +9,10 @@ using namespace std;
 
 
 int n;
-
-int input[1000005];
 int a[1000005];
-int b[1000005];
+//int b[1000005];
+vector<int> b;
 int len;
-
-
-int check(int target){
-  int st=0; 
-  int en = len;
-  int mid=0;
-
-  while(1){
-    mid = (st+en)/2;
-
-    if(b[mid]<target){
-      st = mid+1;
-
-    }else if(b[mid] > target){
-      en = mid-1;
-
-    }else{
-      break;
-    }
-  }
-  return mid;
-
-}
 
 
 int main(void){ 
@@ -47,28 +23,21 @@ int main(void){
 
   for(int i=0; i<n ; i++){
     cin>>a[i];
-    input[i] = a[i];
+    b.push_back(a[i]);
   }
 
-  sort(a, a+n);
+  sort(b.begin(), b.end());
   
   //중복제거 후 b배열로 옮김
-  int minus=0;
-  for(int i=0; i<n ; i++){
-    if(i == n-1 || a[i] != a[i+1]){
-      b[i-minus] = a[i];
-      len++;
-    }else{
-      minus++;
-    } 
-  }
+  b.erase( unique(b.begin(), b.end()), b.end() );
+  
 
   //이제 이분탐색해서 target의 인덱스값 알아내면됨
   for(int i=0; i<n; i++){
 
    //cout<<"input[i]: "<<input[i]<<"\n";
     
-    cout<<check(input[i])<<" ";
+    cout<< lower_bound(b.begin(), b.end(), a[i]) - b.begin()  <<" ";
   }
   
 
