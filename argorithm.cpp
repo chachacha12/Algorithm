f1a1878b0b0f4aa9f5e946d67ef6d216ff8e07ea
chachacha12 <chaminwoo369@gmail.com>
2024-04-23 21:25:37 +0900

    1822 이분탐색 차집합

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,33 +6,44 @@
 #include <vector>
 using namespace std; 
 
-
-int n, m;
+int n,m;
 int a[500005];
+int b[500005];
 
-
+vector<int> v;
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
-  cin>>n;
+  cin>>n>>m;
 
   for(int i=0; i<n; i++){
     cin>>a[i];
   }
-  cin>>m;
 
-  int num;
-  
-  sort(a,a+n);
-
-  while(m--){
-    cin>>num;
-
-    cout << binary_search(a,a+n,num) << ' ';
+  for(int i=0; i<m; i++){
+    cin>>b[i];
   }
-  return 0;
+
+  sort(a,a+n);
+  sort(b, b+m);
+
+
+  for(int i=0; i<n; i++){
+    if(!binary_search(b,b+m,a[i])){
+      v.push_back(a[i]);
+    }
+  }
+
+   cout<<v.size()<<"\n";
+  for(int i=0; i<v.size();i++){
+    cout<<v[i]<<" ";
+  }
+
+
+
+
 }
 
 
