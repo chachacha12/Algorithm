8ea8bdd5cbeb8e08f9973a978014aa217ea04be9
chachacha12 <chaminwoo369@gmail.com>
2024-05-13 02:12:37 +0900

    1149 dp RGB거리

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,45 +6,40 @@
 #include <vector>
 using namespace std; 
 
-
 int n;
-int d[305][2];
-int a[305];
+int d[1005][3];
+int r[1005];
+int g[1005];
+int b[1005];
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
  /*
- 테이블: d[i][0] = i번째까지 갔을때 이전계단을 밟았을때 최댓값 (i번째는 무조건 밟음)
-        d[i][1] = i번째까지 갔을때 이전계단을 안 밟았을때 최댓값 (i번째는 무조건 밟음)
+ 테이블: 
+
 
  점화식: 
 
  */
 
-
-
   cin>>n;
   for(int i=1; i<=n; i++){
-    cin>>a[i];
+    cin>>r[i]>>g[i]>>b[i];
   }
 
-  d[1][0] = a[1];
-  d[1][1] = a[1];
+  d[1][0] = r[1];
+  d[1][1] = g[1];
+  d[1][2] = b[1];
 
-  d[2][0] = a[1]+a[2];
-  d[2][1] = a[2];
-
-
-
-  for(int i=3; i<=n; i++){
-     d[i][0] = d[i-1][1] + a[i];
-     d[i][1] = max(d[i-2][0], d[i-2][1] )+ a[i];
+  for(int i=2; i<=n; i++){
+    d[i][0]  = min(d[i-1][1], d[i-1][2]) +r[i];
+    d[i][1]  = min(d[i-1][0], d[i-1][2]) +g[i];
+    d[i][2]  = min(d[i-1][1], d[i-1][0]) +b[i];
   }
 
-  cout<<max(d[n][0], d[n][1]);
-
+  cout<<min({d[n][0], d[n][1],d[n][2]});
 
 
 
