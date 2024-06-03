32fa818450f29bf554d78830b39a221caf1a0361
chachacha12 <chaminwoo369@gmail.com>
2024-06-03 14:16:22 +0900

    9251 dp LCS

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,38 +6,40 @@
 #include <vector>
 using namespace std; 
 
-
-int n,m;
-int arr[10];
-bool isused[10];
-
-
-void func(int k){
-  if(k==m){
-    for(int i=0; i<m; i++){
-      cout<<arr[i]<<' ';
-    }
-    cout<<'\n';
-    return;
-  } 
-  
-  int st=1;
-  if(k!=0)
-    st = arr[k-1];
-  
-  for(int i=st; i<=n; i++){
-    
-      arr[k]=i; 
-      func(k+1);
-  }
-}
-
+int d[1005][1005];
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
-  cin>>n>>m;
-  func(0);
+
+  string a;
+  string b;
+
+  cin>>a>>b;
+
+  for(int i=0; i<a.length(); i++){
+    d[i][0]=0;
+  }
+  for(int i=0; i<b.length(); i++){
+    d[0][i]=0;
+  }
+
+
+  for(int i=1; i<a.length()+1; i++){
+    for(int j=1; j<b.length()+1; j++){
+      if(a[i-1]==b[j-1]){
+        d[i][j] =  d[i-1][j-1]+1;
+        //cout<<"d[i][j]: "<<d[i][j]<<"\n";
+      }else{
+         d[i][j] = max(d[i-1][j],d[i][j-1]);
+         //cout<<"d[i][j]: "<<d[i][j]<<"\n";
+      }
+    }
+  }
+
+  cout<<d[a.length()][b.length()];
+
+
 }
 
 
