de92000c6d6a2452302ad880f50a9b26a9e14933
chachacha12 <chaminwoo369@gmail.com>
2024-06-06 02:58:19 +0900

    1439 그리디 뒤집기

--- argorithm.cpp
+++ argorithm.cpp
@@ -6,70 +6,48 @@
 #include <vector>
 using namespace std; 
 
-vector <pair<int,int>> house;
-vector<pair<int,int>> chick;
 
-int n,m;
-int board[55][55];
-int ans=0;  //현재 조합일때의 최소거리
-int result=1000000;  //최종적인 최소거리 (정답값)
+string s;
 
 int main(void){ 
   ios::sync_with_stdio(0);
   cin.tie(0); 
 
-  cin>>n>>m;
-  for(int i=1; i<=n; i++){
-    for(int j=1; j<=n; j++){
-      cin>>board[i][j];
+  cin>>s;
+  int zero=0;
+  int one=0;
+  char flag;
 
-      if(board[i][j]==1)
-        house.push_back({i,j});  //집은 여기 넣어줌
-      if(board[i][j]==2)
-        chick.push_back({i,j});  //치킨집은 여기 넣어줌
+  flag = s[0];
+  for(int i=1; i<s.length(); i++){
+    if(s[i] =='0'){
+      if(flag=='1'){
+        flag='0';
+        one++;
+      }
+    }else{
+      if(flag=='0')  //이전까지 0이었으면
+      {
+        flag='1';
+        zero++;    
+      }
     }
   }
-
-
-  //총 치킨집 중 m개를 고르는 조합 방법
-  vector<int> brute(chick.size(), 1);
-  for(int i=0; i<m; i++){
-    brute[i]=0;
+  
+  if(flag=='0'){
+    zero++;
+  }else{
+    one++;
   }
 
-  do{
-       //모든 집을 돌면서 
-        for(int i=0; i<house.size(); i++){
-          int x = house[i].first;
-          int y = house[i].second;
-          int min_dist=100000; //최소 거리
 
-          // cout<<"X: " <<x<<"y: "<<y<<'\n';
-          
-          //모든 치킨 집 중 m개 조합 고르기
-           for(int j = 0; j < chick.size(); j++){
-              if(brute[j] == 0) {//고른 치킨집
-               int x2 = chick[j].first;
-               int y2 = chick[j].second;
+  if((zero==0 && one==1) || (one==0 && zero==1)){
+    cout<<'0';
+  }else{
+    cout<<min(one, zero);
+  }
 
-              //  cout<<"X2: " <<x2<<"y2: "<<y2<<'\n';
-
-                min_dist = min(abs(x-x2) + abs(y-y2), min_dist); 
-
-              }  
-          }
-          // cout<<"min_dist"<<min_dist<<'\n';
-          ans+=min_dist;  //해당 집의 최소거리를 합에 더해줌   
-        }
-        //  cout<<"\n";
-        // cout<<"ans: "<<ans<<'\n';
-        // cout<<"\n";
-        result = min(result, ans);
-        ans=0;
-  }while(next_permutation(brute.begin(), brute.end()));
-
-  cout<<result;
-
+  
 
 }
 
--- tempCodeRunnerFile.cpp
+++ tempCodeRunnerFile.cpp
@@ -1,2 +1,64 @@
+#include <iostream>
+#include <math.h>
+#include <algorithm>
+#include <stack>
+#include <queue>
+#include <vector>
+using namespace std; 
 
-    
+
+string s;
+
+int main(void){ 
+  ios::sync_with_stdio(0);
+  cin.tie(0); 
+
+  cin>>s;
+  int zero=0;
+  int one=0;
+  char flag;
+
+  flag = s[0];
+  for(int i=1; i<s.length(); i++){
+    if(s[i] =='0'){
+      if(flag=='1'){
+        flag='0';
+        one++;
+      }
+    }else{
+      if(flag=='0')  //이전까지 0이었으면
+      {
+        flag='1';
+        zero++;    
+      }
+    }
+  }
+  
+  if(flag=='0'){
+    zero++;
+  }else{
+    one++;
+  }
+
+
+  if((zero==0 && one==1) || (one==0 && zero==1)){
+    cout<<'0';
+  }else{
+    cout<<min(one, zero);
+  }
+
+  
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
