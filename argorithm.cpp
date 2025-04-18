b344cb0610287ba58bcbd063f9c0dce767f58d4a
chachacha12 <chaminwoo369@gmail.com>
2025-04-19 01:30:23 +0900

    [PCCE 기출문제] 9번 / 지폐 접기

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,41 +1,20 @@
 #include <string>
 #include <vector>
-#include <algorithm>
+
 using namespace std;
 
-int solution(vector<int> mats, vector<vector<string>> park) {
-    int R = park.size();           // 공원의 행 개수
-    int C = park[0].size();        // 공원의 열 개수
-
-    // 가장 큰 돗자리부터 확인하기 위해 내림차순 정렬
-    sort(mats.begin(), mats.end(), greater<int>());
-    
-    // mats에 있는 각 돗자리 크기에 대하여 검사
-    for (int size : mats) {
-        // 만약 돗자리 크기가 park 전체보다 크면 skip
-        if (size > R || size > C) continue;
-        
-        // 가능한 시작 행, 열 위치에 대해 검사
-        for (int i = 0; i <= R - size; i++) {
-            for (int j = 0; j <= C - size; j++) {
-                bool canPlace = true;
-                // 현재 시작점 (i, j)에서 size x size 영역 검증
-                for (int x = i; x < i + size && canPlace; x++) {
-                    for (int y = j; y < j + size; y++) {
-                        if (park[x][y] != "-1") {  // 해당 칸에 사람이 있으면
-                            canPlace = false;
-                            break;
-                        }
-                    }
-                }
-                // 만약 영역이 모두 비어 있다면 해당 크기를 반환
-                if (canPlace) {
-                    return size;
-                }
-            }
+int solution(vector<int> wallet, vector<int> bill) {
+    int w1 = wallet[0], w2 = wallet[1];
+    int b0 = bill[0], b1 = bill[1];
+    int answer = 0;
+    // 지폐가 지갑에 들어갈 때까지 접기
+    while (!((b0 <= w1 && b1 <= w2) || (b1 <= w1 && b0 <= w2))) {
+        if (b0 > b1) {
+            b0 /= 2;
+        } else {
+            b1 /= 2;
         }
+        answer++;
     }
-    
-    // 모든 크기에서 찾지 못하면 -1 반환
-    return -1;
+    return answer;
 }
