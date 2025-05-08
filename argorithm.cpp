e0bc2ab7b75fbdd63baeee8abe50916d2eafedc8
chachacha12 <chaminwoo369@gmail.com>
2025-05-08 23:28:43 +0900

    카카오기출/가장 많이 받은 선물

--- argorithm.cpp
+++ argorithm.cpp
@@ -1,41 +1,59 @@
 #include <string>
 #include <vector>
+#include <unordered_map>
 #include <algorithm>
+
 using namespace std;
 
-int solution(vector<int> mats, vector<vector<string>> park) {
-    int R = park.size();           // 공원의 행 개수
-    int C = park[0].size();        // 공원의 열 개수
+int solution(vector<string> friends, vector<string> gifts) {
+    int n = friends.size();
+    unordered_map<string, int> idx;
+    idx.reserve(n);
+    for (int i = 0; i < n; ++i) {
+        idx[friends[i]] = i;
+    }
 
-    // 가장 큰 돗자리부터 확인하기 위해 내림차순 정렬
-    sort(mats.begin(), mats.end(), greater<int>());
-    
-    // mats에 있는 각 돗자리 크기에 대하여 검사
-    for (int size : mats) {
-        // 만약 돗자리 크기가 park 전체보다 크면 skip
-        if (size > R || size > C) continue;
-        
-        // 가능한 시작 행, 열 위치에 대해 검사
-        for (int i = 0; i <= R - size; i++) {
-            for (int j = 0; j <= C - size; j++) {
-                bool canPlace = true;
-                // 현재 시작점 (i, j)에서 size x size 영역 검증
-                for (int x = i; x < i + size && canPlace; x++) {
-                    for (int y = j; y < j + size; y++) {
-                        if (park[x][y] != "-1") {  // 해당 칸에 사람이 있으면
-                            canPlace = false;
-                            break;
-                        }
-                    }
+    vector<vector<int>> gifts_count(n, vector<int>(n, 0));
+    vector<int> total_given(n, 0), total_received(n, 0);
+
+    for (auto &s : gifts) {
+        auto space_pos = s.find(' ');
+        string a = s.substr(0, space_pos);
+        string b = s.substr(space_pos + 1);
+        int i = idx[a], j = idx[b];
+        gifts_count[i][j]++;
+        total_given[i]++;
+        total_received[j]++;
+    }
+
+    vector<int> gift_index(n);
+    for (int i = 0; i < n; ++i) {
+        gift_index[i] = total_given[i] - total_received[i];
+    }
+
+    vector<int> next_receive(n, 0);
+
+    for (int i = 0; i < n; ++i) {
+        for (int j = i + 1; j < n; ++j) {
+            int a_to_b = gifts_count[i][j];
+            int b_to_a = gifts_count[j][i];
+            if (a_to_b != b_to_a) {
+                if (a_to_b > b_to_a) {
+                    next_receive[i]++;
+                } else {
+                    next_receive[j]++;
                 }
-                // 만약 영역이 모두 비어 있다면 해당 크기를 반환
-                if (canPlace) {
-                    return size;
+            } else {
+                if (gift_index[i] != gift_index[j]) {
+                    if (gift_index[i] > gift_index[j]) {
+                        next_receive[i]++;
+                    } else {
+                        next_receive[j]++;
+                    }
                 }
             }
         }
     }
-    
-    // 모든 크기에서 찾지 못하면 -1 반환
-    return -1;
+
+    return *max_element(next_receive.begin(), next_receive.end());
 }
